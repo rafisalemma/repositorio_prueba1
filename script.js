3dad06997f22d0f0803e666edc56a36f62e01091
unknown <rafaela.salemma@gmail.com>
2023-09-11 10:51:21 -0400

    prueba inicial de repositorio

---
+++ script.js
@@ -0,0 +1,27 @@
+//let contenedorDerecho = document.getElementById('contenedor-derecho')
+//console.log(contenedorDerecho)
+
+//contenedorDerecho.style.backgroundColor ='purple'
+
+arrastrarElemento(document.getElementById('plaza1'));
+arrastrarElemento(document.getElementById('plaza2'));
+arrastrarElemento(document.getElementById('plaza3'));
+arrastrarElemento(document.getElementById('plaza4'));
+arrastrarElemento(document.getElementById('plaza5'));
+arrastrarElemento(document.getElementById('plaza6'));
+arrastrarElemento(document.getElementById('plaza7'));
+arrastrarElemento(document.getElementById('plaza8'));
+arrastrarElemento(document.getElementById('plaza9'));
+arrastrarElemento(document.getElementById('plaza10'));
+arrastrarElemento(document.getElementById('plaza11'));
+arrastrarElemento(document.getElementById('plaza12'));
+arrastrarElemento(document.getElementById('plaza13'));
+arrastrarElemento(document.getElementById('plaza14'));
+arrastrarElemento(document.getElementById('plaza15'));
+arrastrarElemento(document.getElementById('plaza16'));
+arrastrarElemento(document.getElementById('plaza17'));
+arrastrarElemento(document.getElementById('plaza18'));
+
+function arrastrarElemento(){
+    console.log("Hola")
+}
